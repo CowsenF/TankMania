7496e60b6f310ddb664f1887eb1b9772ac1a2b47
Cowsen-barebar <markuspedersen13@gmail.com>
2021-11-17 10:05:16 +0100

    player Class

---
+++ player.js
@@ -0,0 +1,31 @@
+class Player{
+
+    constructor(){
+        this.color = color(0);
+        this.x;
+        this.y;
+        this.front = Vector();
+        this.speed;
+        this.score;
+    }
+
+    show(){
+
+
+
+
+    }
+
+    spawnBullet(){
+
+
+    }
+
+    move(){
+
+        
+    }
+
+
+
+}
