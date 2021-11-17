0218650f8deeb0f27ec495de5175cf54597dcd90
Cowsen-barebar <markuspedersen13@gmail.com>
2021-11-17 10:39:22 +0100

    update to Player class

--- player.js
+++ player.js
@@ -1,17 +1,20 @@
 class Player{
 
-    constructor(){
+    constructor(image){
         this.color = color(0);
-        this.x;
-        this.y;
-        this.front = Vector();
+        this.x = 0;
+        this.y = 50;
+        this.angle;
         this.speed;
         this.score;
+        this.img = image;
     }
 
     show(){
-
-
+        translate((this.x), (this.y));
+        rotate(this.angle);
+        imageMode(CENTER);
+        image(this.img,this.x,this.y,20,20)
 
 
     }
@@ -24,6 +27,7 @@ class Player{
     move(){
 
         
+
     }
 
 
--- sketch.js
+++ sketch.js
@@ -1,7 +1,18 @@
+let player1;
+let blackTankImg;
+
+function preload() {
+  
+  blackTankImg = loadImage("black_tank.png");
+
+}
+
 function setup() {
   createCanvas(400, 400);
+  player1 = new Player(blackTankImg);
 }
 
 function draw() {
   background(220);
+  player1.show();
 }
